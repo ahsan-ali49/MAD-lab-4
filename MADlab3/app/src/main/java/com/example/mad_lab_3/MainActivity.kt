f7ada830ec6cc265d2130f17cf2226dc76df5864
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-04-23 23:49:09 +0500

    First commit

--- MADlab3/app/src/main/java/com/example/mad_lab_3/MainActivity.kt
+++ MADlab3/app/src/main/java/com/example/mad_lab_3/MainActivity.kt
@@ -3,16 +3,11 @@ package com.example.mad_lab_3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mad_lab_3.ui.theme.MADlab3Theme
@@ -22,26 +17,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MADlab3Theme {
-                MainPage()
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
             }
         }
     }
 }
 
 @Composable
-fun MainPage() {
-    Box (
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
-        Column( 
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "Task 1")
-            }
-        }
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MADlab3Theme {
+        Greeting("Android")
     }
 }
